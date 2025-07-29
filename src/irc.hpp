0681b1987593c6fd310d13920428dc586d5feb33
paularynty <paula.rynty@gmail.com>
2025-07-29 19:54:25 +0300

    wadd isValidChannelName

--- src/irc.hpp
+++ src/irc.hpp
@@ -5,5 +5,10 @@
 #define ANSI_RESET "\033[0;39m"
 
 std::string generateRandomId();
+
+//parser.cpp
 int validateArgs(int argc, char **argv);
+
+//utils.cpp
+bool isValidChannelName(const std::string& name);
 void validateClientName(std::string name);
--- src/utils.cpp
+++
@@ -1,2 +0,0 @@
-#include "irc.hpp"
-
