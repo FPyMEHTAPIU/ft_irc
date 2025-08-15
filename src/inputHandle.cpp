bd81da1b95fc7389f8567254162951d1b7bbc430
Nick Saveliev <nick.saveliev@reaktor.com>
2025-08-15 20:41:12 +0300

    Add commands switcher.

--- src/inputHandle.cpp
+++ src/inputHandle.cpp
@@ -1,7 +1,67 @@
 #include "irc.hpp"
 
-void handleInput(const std::string &input)
+void handleInput(const std::string &input, Server *server)
 {
 	if (input.empty())
 		return;
+
+	std::vector<std::string> args = split(input, ' ');
+	validateCommand(args[0]);
+
+	switch (hash(input.c_str(), input.size()))
+	{
+	case hash("pass"):
+		break;
+	case hash("nick"):
+		break;
+	case hash("user"):
+		break;
+	case hash("join"):
+	{
+		std::cout << "privet" << std::endl;
+		return;
+		auto it = std::find_if(
+				server->getChannels().begin(),
+				server->getChannels().end(),
+				[&](const std::shared_ptr<Channel> &ch)
+				{
+					return ch->getName() == args[1];
+				});
+
+		if (it == server->getChannels().end())
+		{
+			std::shared_ptr<Channel> newChannel = std::make_shared<Channel>(args[1], *server->getClients().begin());
+			server->addChannel(newChannel);
+		}
+	}
+	break;
+	case hash("part"):
+		break;
+	case hash("privmsg"):
+		break;
+	case hash("notice"):
+		break;
+	case hash("mode"):
+		break;
+	case hash("invite"):
+		break;
+	case hash("kick"):
+		break;
+	case hash("topic"):
+		break;
+	case hash("names"):
+		break;
+	case hash("list"):
+		break;
+	case hash("oper"):
+		break;
+	case hash("kill"):
+		break;
+	case hash("quit"):
+		break;
+	case hash("msg"):
+		break;
+	default:
+		break;
+	}
 }
--- src/irc.hpp
+++ src/irc.hpp
@@ -1,13 +1,18 @@
 #pragma once
 #include <iostream>
 #include <set>
-#include <bits/stdc++.h>
+#include <vector>
+#include "Server.hpp"
+#include "client/Client.hpp"
+#include "channel/Channel.hpp"
 
 #define ANSI_RED "\033[0;91m"
 #define ANSI_RESET "\033[0;39m"
 
 #define PWD_MIN_LENGTH 5
 #define PWD_MAX_LENGTH 32
+
+class Server;
 
 // parser.cpp
 void validatePassword(const std::string &password);
@@ -18,3 +23,24 @@ void validateCommand(const std::string &command);
 
 // utils/
 bool isValidChannelName(const std::string &name);
+std::vector<std::string> split(std::string sentence, char sep);
+
+void handleInput(const std::string &input, Server *server);
+
+// These `hash` fucntions are for using `std::string` in `switch`
+constexpr uint32_t hash(const char *data, size_t size) noexcept
+{
+  uint32_t h = 5381;
+  for (const char *c = data; c < data + size; ++c)
+    h = ((h << 5) + h) + static_cast<unsigned char>(*c);
+  return h;
+}
+
+// These `hash` fucntions are for using `std::string` in `switch`
+constexpr uint32_t hash(const char *str) noexcept
+{
+  size_t len = 0;
+  while (str[len] != '\0')
+    ++len;
+  return hash(str, len);
+}
