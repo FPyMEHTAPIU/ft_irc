94cbdbd078062a8ccfda335682ef8f970933c37f
Nick Saveliev <131614798+FPyMEHTAPIU@users.noreply.github.com>
2025-09-28 17:28:23 +0300

    Revert "Fix nick command to allow nickname changing."
    This reverts commit a61ddf88aff72f8f4fa19779b89e42f823adfbe9.

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -1,39 +1,9 @@
 #include "commands.hpp"
 
-std::string handleNick(Server *server, std::shared_ptr<Client> client, const std::string &newNick)
+std::string handleNick(std::shared_ptr<Client> client, const std::string &nick)
 {
-	if (newNick.empty())
-		return "431 :No nickname given\r\n";
-
-	for (auto &c : server->getClients())
-	{
-		if (c.second->getNick() == newNick && c.second != client)
-		{
-			return "433 " + newNick + " :Nickname is already in use\r\n";
-		}
-	}
-
-	std::string oldNick = client->getNick();
-
-	if (oldNick.empty())
-	{
-		client->setNick(newNick);
-		std::string prefix = generatePrefix(client);
-		std::string result = prefix + " NICK registered\r\n";
-		return result;
-	}
-
+	client->setNick(nick);
 	std::string prefix = generatePrefix(client);
-	client->setNick(newNick);
-
-	std::string msg = prefix + " NICK :" + newNick + "\r\n";
-	for (auto &ch : server->getChannels())
-	{
-		if (ch.second->hasUser(client))
-		{
-			ch.second->broadcast(server, msg, client->getFd());
-		}
-	}
-
-	return msg;
+	std::string result = prefix + " NICK registered\r\n";
+	return result;
 }
